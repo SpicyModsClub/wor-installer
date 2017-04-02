8c57d95056f0ac032184282dd633da9d6a68206b
Iris Ward <aditu.venyhandottir@gmail.com>
2017-04-02 22:47:18 +0100

    Initial commit

---
+++ WoR Installer/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WoR_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.Label label1;
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(106, 13);
+            label1.TabIndex = 1;
+            label1.Text = "GH3 Install Location:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(124, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(266, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(396, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(30, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInstall.Location = new System.Drawing.Point(182, 49);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.TabIndex = 3;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "gh3.exe";
+            this.ofd.Filter = "Guitar Hero 3 Executable (gh3.exe)|*.exe";
+            this.ofd.InitialDirectory = "C:\\Program Files (x86)\\";
+            this.ofd.ReadOnlyChecked = true;
+            this.ofd.RestoreDirectory = true;
+            this.ofd.Title = "Locate Guitar Hero III";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnInstall;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 84);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.txtPath);
+            this.MaximumSize = new System.Drawing.Size(9001, 122);
+            this.MinimumSize = new System.Drawing.Size(300, 122);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "WoR Mod Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.OpenFileDialog ofd;
+    }
+}
+
---
+++ WoR Installer/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+
+namespace WoR_Installer
+{
+    public partial class Form1 : Form
+    {
+        private string installLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.installLocation =
+                    (string)
+                    Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Aspyr\Guitar Hero III", "Path", null);
+
+                if (string.IsNullOrEmpty(this.installLocation))
+                {
+                    // Not likely but people might still be running 32-bit; try that
+                    this.installLocation =
+                        (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Aspyr\Guitar Hero III", "Path", null);
+                }
+            }
+            catch (SecurityException)
+            {
+                MessageBox.Show(
+                    "You don't have permissions to read from the registry!  Please make sure you are running as administrator.");
+            }
+            catch
+            {
+                // Can't find the install location; the user can locate it themself
+            }
+
+            if (!string.IsNullOrEmpty(this.installLocation))
+            {
+                this.txtPath.Text = this.installLocation;
+                this.ofd.InitialDirectory = this.installLocation;
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK != ofd.ShowDialog())
+            {
+                return;
+            }
+
+            this.installLocation = Path.GetDirectoryName(this.ofd.FileName);
+            this.txtPath.Text = this.installLocation;
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            ((Control)sender).Enabled = false;
+
+            var di = new DirectoryInfo(this.installLocation);
+            int failedFiles = 0;
+
+            try
+            {
+                foreach (var f in di.GetFiles("*", SearchOption.AllDirectories))
+                {
+                    try
+                    {
+                        f.Attributes &= ~FileAttributes.ReadOnly;
+                    }
+                    catch
+                    {
+                        failedFiles++;
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Installation directory not found.");
+                ((Control)sender).Enabled = true;
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                ((Control) sender).Enabled = true;
+                return;
+            }
+
+            if (failedFiles > 0) { 
+                var dr = MessageBox.Show(
+                                "Did not have permissions to set some files to writable.  The mod may not install correctly.  Please make sure you are running as administrator.  Try to continue anyway?",
+                                "Permissions Error",
+                                MessageBoxButtons.YesNo);
+
+                if (dr == DialogResult.No) {
+                    ((Control) sender).Enabled = true;
+                    return;
+                }
+            }
+
+            var arguments = string.Format("x -o\"{0}\" -y Data.7z", this.installLocation);
+            ProcessStartInfo info = new ProcessStartInfo("7za.exe", arguments);
+            try
+            {
+               var extractor = Process.Start(info);
+
+                Cursor = Cursors.WaitCursor;
+                extractor.WaitForExit();
+                Cursor = Cursors.Default;
+
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                ((Control) sender).Enabled = true;
+                return;
+            }
+
+            MessageBox.Show("Installation complete!");
+            ((Control) sender).Enabled = true;
+        }
+    }
+}
